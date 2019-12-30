0c9447f27c8621cc30182be90598a7b964d9538b
Asha <asha.vakkattil@gmail.com>
2019-12-30 19:16:49 +0530

    App Header and App Footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-footer color="grey lighten-4">
+    <v-spacer></v-spacer>
+    <p>Holidays@2019</p>
+  </v-footer>
+</template>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <v-app-bar app dense color="black" dark>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Holiday</v-toolbar-title>
+  </v-app-bar>
+</template>
